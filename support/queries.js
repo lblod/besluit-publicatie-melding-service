fa139af1eb06e2caab8d9992f5542c2d97268c98
Claire Lovisa <lovisa.claire@gmail.com>
2020-03-25 14:56:43 +0100

    Filter publication resources requirering melding on besluit types

--- support/queries.js
+++ support/queries.js
@@ -8,6 +8,25 @@ const DEFAULT_GRAPH = (process.env || {}).DEFAULT_GRAPH || 'http://mu.semte.ch/g
 const PENDING_STATUS = "http://lblod.data.gift/besluit-publicatie-melding-statuses/ongoing";
 const FAILED_STATUS = "http://lblod.data.gift/besluit-publicatie-melding-statuses/failure";
 const SUCCESS_STATUS = "http://lblod.data.gift/besluit-publicatie-melding-statuses/success";
+const BESLUIT_TYPES_MELDING = [
+  '<https://data.vlaanderen.be/id/concept/BesluitType/67378dd0-5413-474b-8996-d992ef81637a>', // Reglementen en verordeningen
+  '<https://data.vlaanderen.be/id/concept/BesluitType/0d1278af-b69e-4152-a418-ec5cfd1c7d0b>', // Aanvullend reglement op het wegverkeer m.b.t. gemeentewegen in speciale beschermingszones
+  '<https://data.vlaanderen.be/id/concept/BesluitType/256bd04a-b74b-4f2a-8f5d-14dda4765af9>', // Tijdelijke politieverordening (op het wegverkeer)
+  '<https://data.vlaanderen.be/id/concept/BesluitType/25deb453-ae3e-4d40-8027-36cdb48ab738>', // Deontologische Code
+  '<https://data.vlaanderen.be/id/concept/BesluitType/3bba9f10-faff-49a6-acaa-85af7f2199a3>', // Aanvullend reglement op het wegverkeer m.b.t. gemeentewegen in havengebied
+  '<https://data.vlaanderen.be/id/concept/BesluitType/4673d472-8dbc-4cea-b3ab-f92df3807eb3>', // Personeelsreglement
+  '<https://data.vlaanderen.be/id/concept/BesluitType/4d8f678a-6fa4-4d5f-a2a1-80974e43bf34>', // Aanvullend reglement op het wegverkeer enkel m.b.t. gemeentewegen (niet in havengebied of speciale beschermingszones)
+  '<https://data.vlaanderen.be/id/concept/BesluitType/5ee63f84-2fa0-4758-8820-99dca2bdce7c>', // Delegatiereglement
+  '<https://data.vlaanderen.be/id/concept/BesluitType/7d95fd2e-3cc9-4a4c-a58e-0fbc408c2f9b>', // Aanvullend reglement op het wegverkeer m.b.t. één of meerdere gewestwegen
+  '<https://data.vlaanderen.be/id/concept/BesluitType/84121221-4217-40e3-ada2-cd1379b168e1>', // Andere
+  '<https://data.vlaanderen.be/id/concept/BesluitType/a8486fa3-6375-494d-aa48-e34289b87d5b>', // Huishoudelijk reglement
+  '<https://data.vlaanderen.be/id/concept/BesluitType/ba5922c9-cfad-4b2e-b203-36479219ba56>', // Retributiereglement
+  '<https://data.vlaanderen.be/id/concept/BesluitType/d7060f97-c417-474c-abc6-ef006cb61f41>', // Subsidie, premie, erkenning
+  '<https://data.vlaanderen.be/id/concept/BesluitType/e8aee49e-8762-4db2-acfe-2d5dd3c37619>', // Reglement Onderwijs
+  '<https://data.vlaanderen.be/id/concept/BesluitType/e8afe7c5-9640-4db8-8f74-3f023bec3241>', // Politiereglement
+  '<https://data.vlaanderen.be/id/concept/BesluitType/efa4ec5a-b006-453f-985f-f986ebae11bc>', // Belastingreglement
+  '<https://data.vlaanderen.be/id/concept/BesluitType/fb92601a-d189-4482-9922-ab0efc6bc935>'  // Gebruikersreglement
+];
 
 async function getTaskForResource(publishedResource){
   let q = `
@@ -199,16 +218,21 @@ async function requiresMelding(resource){
   let queryStr = `
     PREFIX ext: <http://mu.semte.ch/vocabularies/ext/>
     PREFIX prov: <http://www.w3.org/ns/prov#>
+    PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
 
-     SELECT DISTINCT ?type WHERE{
-
-       GRAPH ?graph {
+     SELECT DISTINCT ?documentType WHERE{
+       GRAPH ?g {
          ?extractedResource prov:wasDerivedFrom ${sparqlEscapeUri(resource)}.
-         ?extractedResource a ?type.
+         ?extractedResource a ?documentType.
+         ?behandeling prov:generated ?besluit .
+         ?besluit rdf:type ?besluitType .
        }
-       FILTER(
-         ?type IN ( ext:Notulen, ext:Agenda, ext:Besluitenlijst)
-       )
+       GRAPH ?h {
+         ${sparqlEscapeUri(resource)} ext:publishesBehandeling ?versionedBehandeling .
+         ?versionedBehandeling ext:behandeling ?behandeling .
+       }
+       FILTER( ?documentType IN ( ext:Notulen, ext:Uittreksel ) )
+       FILTER( ?besluitType IN ( ${BESLUIT_TYPES_MELDING.join(', ')} ) )
     }
   `;
   let res = await query(queryStr);
